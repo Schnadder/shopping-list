c60d603be7d79ff5bf62ad7c83553e3f075fc05b
Sander Sandvik Nessa <sandesn@stud.ntnu.no>
2025-11-17 19:34:28 +0100

    add hold to reorder

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,21 +1,39 @@
 <template>
   <ion-list>
-    <ion-item
-      v-for="item in sortedItems"
-      :key="item.id"
-      button
-      @click="$emit('toggle-done', item.id)"
-    >
-      <ion-label :class="{ done: item.done }">
-        {{ item.text }}
-      </ion-label>
-    </ion-item>
+    <!-- UNFINISHED ITEMS -->
+    <ion-reorder-group :disabled="false" @ionItemReorder="ev => onReorder(ev, false)">
+      <ion-reorder v-for="item in unfinishedItems" :key="item.id">
+        <ion-item button @click="$emit('toggle-done', item.id)">
+          <ion-label>
+            {{ item.text }}
+          </ion-label>
+        </ion-item>
+      </ion-reorder>
+    </ion-reorder-group>
+
+    <!-- DONE ITEMS -->
+    <ion-reorder-group v-if="doneItems.length" :disabled="false" @ionItemReorder="ev => onReorder(ev, true)">
+      <ion-reorder v-for="item in doneItems" :key="item.id">
+        <ion-item button @click="$emit('toggle-done', item.id)">
+          <ion-label class="done">
+            {{ item.text }}
+          </ion-label>
+        </ion-item>
+      </ion-reorder>
+    </ion-reorder-group>
   </ion-list>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
-import { IonList, IonItem, IonLabel } from '@ionic/vue'
+import {
+  IonList,
+  IonItem,
+  IonLabel,
+  IonReorderGroup,
+  IonReorder,
+} from '@ionic/vue'
+import type { ItemReorderEventDetail } from '@ionic/vue'
 
 interface ListItem {
   id: string
@@ -28,16 +46,38 @@ const props = defineProps<{
   items: ListItem[]
 }>()
 
-defineEmits<{
+const emit = defineEmits<{
   (e: 'toggle-done', id: string): void
   (e: 'reorder', newOrder: ListItem[]): void
 }>()
 
-const sortedItems = computed(() => {
-  const unfinished = props.items.filter(i => !i.done)
-  const done = props.items.filter(i => i.done)
-  return [...unfinished, ...done]
-})
+const unfinishedItems = computed(() => props.items.filter(i => !i.done))
+const doneItems = computed(() => props.items.filter(i => i.done))
+
+function reorderArray(arr: ListItem[], from: number, to: number): ListItem[] {
+  const copy = [...arr]
+  const [moved] = copy.splice(from, 1)
+  copy.splice(to, 0, moved)
+  return copy
+}
+
+function onReorder(
+  ev: CustomEvent<ItemReorderEventDetail>,
+  isDoneGroup: boolean,
+) {
+  const from = ev.detail.from
+  const to = ev.detail.to
+
+  const currentUnfinished = unfinishedItems.value
+  const currentDone = doneItems.value
+  const newUnfinished = isDoneGroup ? currentUnfinished : reorderArray(currentUnfinished, from, to)
+  const newDone = isDoneGroup ? reorderArray(currentDone, from, to): currentDone
+  const merged = [...newUnfinished, ...newDone]
+
+  emit('reorder', merged)
+
+  ev.detail.complete()
+}
 </script>
 
 <style scoped>
